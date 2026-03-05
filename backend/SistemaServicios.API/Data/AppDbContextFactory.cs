51b48624cc5f729cff25d3ac60990d5b806ff673
Efrén Campuzano <efrecamp@hotmail.com>
2026-03-05 15:33:56 -0700

    Format Npgsql connection string for readability

--- backend/SistemaServicios.API/Data/AppDbContextFactory.cs
+++ backend/SistemaServicios.API/Data/AppDbContextFactory.cs
@@ -13,7 +13,9 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Database=design_time_db;Username=postgres;Password=postgres");
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Database=design_time_db;Username=postgres;Password=postgres"
+        );
         return new AppDbContext(optionsBuilder.Options);
     }
 }
